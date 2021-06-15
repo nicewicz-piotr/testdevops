8b2179a0cdb776ad62519d65b2bb94037db8dc7b
nicewicz-piotr <72817730+nicewicz-piotr@users.noreply.github.com>
2021-06-15 15:02:36 -0500

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace pipelines_dotnet_core.Controllers
             return View();
         }
 
-        public IActionResult Privacy()xx
+        public IActionResult Privacy()
         {
             return View();
         }
